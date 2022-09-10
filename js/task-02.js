c2d91f92ad96c7e114553e4ffd8ae0fdf305b2ab
Oleksii Nedohybchenko <104201289+aleksnedo@users.noreply.github.com>
2022-09-11 00:50:32 +0300

    task 03

--- js/task-02.js
+++ js/task-02.js
@@ -1,8 +1,22 @@
 const ingredients = [
-  'Potatoes',
-  'Mushrooms',
-  'Garlic',
-  'Tomatos',
-  'Herbs',
-  'Condiments',
+  "Potatoes",
+  "Mushrooms",
+  "Garlic",
+  "Tomatos",
+  "Herbs",
+  "Condiments",
 ];
+
+const ingredientsRef = document.querySelector("#ingredients");
+
+const elements = ingredients.map((ingredient) => {
+  const itemElem = document.createElement("li");
+  itemElem.classList.add("item");
+  itemElem.textContent = ingredient;
+
+  return itemElem;
+});
+
+ingredientsRef.append(...elements);
+
+console.log(elements);
--- js/task-03.js
+++ js/task-03.js
@@ -1,14 +1,26 @@
 const images = [
   {
-    url: 'https://images.pexels.com/photos/140134/pexels-photo-140134.jpeg?auto=compress&cs=tinysrgb&dpr=2&h=750&w=1260',
-    alt: 'White and Black Long Fur Cat',
+    url: "https://images.pexels.com/photos/140134/pexels-photo-140134.jpeg?auto=compress&cs=tinysrgb&dpr=2&h=750&w=1260",
+    alt: "White and Black Long Fur Cat",
   },
   {
-    url: 'https://images.pexels.com/photos/213399/pexels-photo-213399.jpeg?auto=compress&cs=tinysrgb&dpr=2&h=750&w=1260',
-    alt: 'Orange and White Koi Fish Near Yellow Koi Fish',
+    url: "https://images.pexels.com/photos/213399/pexels-photo-213399.jpeg?auto=compress&cs=tinysrgb&dpr=2&h=750&w=1260",
+    alt: "Orange and White Koi Fish Near Yellow Koi Fish",
   },
   {
-    url: 'https://images.pexels.com/photos/219943/pexels-photo-219943.jpeg?auto=compress&cs=tinysrgb&dpr=2&h=750&w=1260',
-    alt: 'Group of Horses Running',
+    url: "https://images.pexels.com/photos/219943/pexels-photo-219943.jpeg?auto=compress&cs=tinysrgb&dpr=2&h=750&w=1260",
+    alt: "Group of Horses Running",
   },
 ];
+
+const imagesRef = document.querySelector(".gallery");
+imagesRef.style.listStyle = "none";
+imagesRef.style.display = "flex";
+
+const listItems = images.map(({ url, alt }) => {
+  return `<li><img src="${url}" alt="${alt}"></li>`;
+});
+
+const markup = listItems.join("");
+
+imagesRef.insertAdjacentHTML("afterbegin", markup);
