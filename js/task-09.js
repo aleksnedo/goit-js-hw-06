cee19eee8b18e5489a7018fb73b8a04ce62b5b93
Oleksii Nedohybchenko <104201289+aleksnedo@users.noreply.github.com>
2022-09-12 16:24:44 +0300

    task 09

--- js/task-09.js
+++ js/task-09.js
@@ -1,3 +1,16 @@
+const bodyRef = document.querySelector("body");
+const colorRef = document.querySelector(".color");
+const buttonChangeColorRef = document.querySelector(".change-color");
+
+const onChangeColor = () => {
+  const currentColor = getRandomHexColor();
+  bodyRef.style.backgroundColor = currentColor;
+  colorRef.textContent = currentColor;
+  console.log(currentColor);
+};
+
+buttonChangeColorRef.addEventListener("click", onChangeColor);
+
 function getRandomHexColor() {
   return `#${Math.floor(Math.random() * 16777215).toString(16)}`;
 }
